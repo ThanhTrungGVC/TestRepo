0e55b88e735b4ebca8b0a6862d2f2fd1b7fdcafa
Nguyễn Thành Trung <44042704+ThanhTrungGVC@users.noreply.github.com>
2019-03-20 19:50:10 +0700

    form login

---
+++ nhom-6/PHP_ShopWeb/login/checklogin.php
@@ -0,0 +1,31 @@
+
+<?php
+session_start();
+
+    $con = mysqli_connect('localhost', 'root', '24021999') or die('Không thể kết nối CSDL: ');
+	$selected = mysqli_select_db($con,"n6shop_database") or die ("Không chọn được database");//chọn database đã tạo tên là demo
+	
+	$u = $p = "";
+	$u = $_POST['username'];
+	$p = md5($_POST['password']);
+
+	$query = mysqli_query($con , "SELECT username, password FROM user WHERE username='$u'");
+
+    if (mysqli_num_rows($query) == 0) {
+        echo "Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
+
+    $row = mysqli_fetch_array($query);
+     
+    //So sánh 2 mật khẩu có trùng khớp hay không
+    if ($p != $row['password']) {
+        echo "Mật khẩu không đúng. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    } else {
+    	header('Location: /PHP_ShopWeb');
+		$_SESSION['us'] = $row['username'];
+    }
+
+    mysql_close();
+?>
